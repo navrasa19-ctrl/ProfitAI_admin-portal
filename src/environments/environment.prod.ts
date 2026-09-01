57464715886f7924440041fae252164e2b3ea8cb
ankitdoi--coder <ankitdoi82@gmail.com>
2026-09-01 13:48:06 +0530

    update:cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://trading-app-new-region.onrender.com/api',
-  wsUrl: 'wss://trading-app-new-region.onrender.com/ws-live-prices'
+  apiUrl: 'https://profitaibot.tech/api',
+  wsUrl: 'wss://profitaibot.tech/ws-live-prices'
 };
